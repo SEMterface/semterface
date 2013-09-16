b372588e4ee38f2b07bdda77cdbff941dc7c45eb
Bret Comnes <bcomnes@gmail.com>
2013-09-16 11:00:22 -0700

    Started webrtc Prototype code

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ SEMterface
 ==========
 
 This is the eventual home for a web based interface for the Jeol JXA-6400 SEM in SRTC 169.
+
+### Quick links
+[Arduino FTDI Drives for Older than Uno Arduino's](http://www.ftdichip.com/Drivers/VCP.htm)
---
+++ prototypes/Arduino_node/arduino_node_test/arduino_node_test.ino
@@ -0,0 +1,20 @@
+const int outputPin = 13;
+ 
+void setup()
+{
+  pinMode(outputPin, OUTPUT);
+  Serial.begin(9600);
+}
+ 
+void loop()
+{
+  if (Serial.available() > 0) {
+    int incomingByte = Serial.read();
+ 
+    if (incomingByte == 0x01) {
+      digitalWrite(outputPin, HIGH);
+    } else if (incomingByte == 0x00) {
+      digitalWrite(outputPin, LOW);
+    }
+  }
+}
