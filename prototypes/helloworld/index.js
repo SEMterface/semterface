29c67b7848ea8054d1eedbeebe3f4fb90248801a
Bret Comnes <bcomnes@gmail.com>
2013-09-17 14:44:34 -0700

    Committing socket.io test

---
+++ prototypes/helloworld/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var port = 3700;
+ 
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+app.use(express.static(__dirname + '/public')); 
+var io = require('socket.io').listen(app.listen(port));
+console.log("Listening on port " + port);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
