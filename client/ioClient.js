953ec752ae6df60b75722aebefb828401e292173
Bret Comnes <bcomnes@gmail.com>
2015-02-05 15:19:20 -0800

    Video box is now flowing correctly.

--- client/ioClient.js
+++ client/ioClient.js
@@ -1,9 +1,20 @@
 var io = require('socket.io-client');
 var $ = require('jquery');
-var webrtc = require('simplewebrtc');
+var SimpleWebRTC = require('simplewebrtc');
 
 var socket = io.connect();
-socket.on('news', function (data) {
+socket.on('news', function(data) {
   console.log(data);
-  socket.emit('my other event', { my: 'data' });
+  socket.emit('my other event', {
+    my: 'data'
+  });
+});
+
+var webrtc = new SimpleWebRTC({
+  // the id/element dom element that will hold "our" video
+  localVideoEl: 'localVideo',
+  // the id/element dom element that will hold remote videos
+  remoteVideosEl: 'remoteVideos',
+  // immediately ask for camera access
+  autoRequestMedia: true
 });
