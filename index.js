5e95bf0222b5d84b8daa8258e1b460c0a88c0642
Bret Comnes <bcomnes@gmail.com>
2015-10-27 22:00:49 -0700

    Got css working

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ var React = require('react')
 var d = require('jsnox')(React)
 var ReactDOM = require('react-dom')
 
+var styles = require('./css/main.css')
+
 // tutorial1.js
 var CommentBox = React.createClass({
   render: function () {
     return (
-      d('div', {className: 'commentBox'}, 'Beep, world! I am a CommentBox.')
+      d(`div.commentBox.${styles.test}^`, {}, 'Beep, world! I am a commentbox.')
     )
   }
 })
 ReactDOM.render(
   d(CommentBox),
-  document.body
+  document.getElementById('content')
 )
