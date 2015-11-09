a102072162abd896c8e851cd21491c8ac4e8a669
Bret Comnes <bcomnes@gmail.com>
2015-11-08 22:14:57 -0800

    Started to Modularize pv/sp control

--- components/Controls/index.js
+++ components/Controls/index.js
@@ -6,7 +6,7 @@ var style = require('./controls.css')
 var Controls = React.createClass({
   render: function render () {
     return d('div', {className: style.controls},
-      d(Control, {name: 'test control'})
+      d(SpMeterControl, {name: 'SpControl'})
     )
   }
 })
@@ -26,11 +26,77 @@ var Control = React.createClass({
           )
         ),
         d('div', {className: style.numeric},
-          d('label', {htmlFor: 'pv'}, 'pv'), d('input[type=text][readOnly]', {id: 'pv', style: {color: '#FF410D'}}),
-          d('label', {htmlFor: 'sp'}, 'sp'), d('input[type=text]', {id: 'sp', style: {color: '#267fb5'}})
+          d('label', {htmlFor: 'pv'}, 'pv'),
+          d('input[type=text][readOnly]', {id: 'pv', style: {color: '#FF410D'}}),
+          d('label', {htmlFor: 'sp'}, 'sp'),
+          d('input[type=text]', {id: 'sp', style: {color: '#267fb5'}})
         )
       )
     )
+  }
+})
+
+var SpMeterControl = React.createClass({
+  render: function render () {
+    return d(BaseControl, {name: this.props.name},
+      d('div', {className: style.meterContainer},
+        d(MeterDisplay,
+          d(MeterNeedle, {color: '#267fb5', key: 'sp', position: 20}),
+          d(MeterNeedle, {color: '#FF410D', key: 'pv', position: 21})
+        ),
+        d(TextIndicator, {color: '#267fb5', name: 'pv', readOnly: true}),
+        d(TextIndicator, {color: '#FF410D', name: 'sp', readOnly: false})
+      )
+    )
+  }
+})
+
+var BaseControl = React.createClass({
+  render: function render () {
+    return d('div', {className: style.baseControl},
+      d('div.name', this.props.name),
+      this.props.children
+    )
+  }
+})
+
+var MeterDisplay = React.createClass({
+  render: function render () {
+    return d('div', {className: style.meterDisplay }, this.props.children)
+  }
+})
+
+var TextIndicator = React.createClass({
+  render: function render () {
+    return d('label', this.props.name,
+      d('input[type=text]', {readOnly: this.props.readOnly,  style: {color: this.props.color}})
+    )
+  }
+})
+
+var RangeInput = React.createClass({
+  render: function render () {
+    return d('div', {className: style.rangeContainer},
+      d('input[type=range]', {
+        className: style.rangeStyle,
+        value: this.props.value,
+        min: this.props.min,
+        max: this.props.max,
+        step: this.props.step
+      })
+    )
+  }
+})
+
+var MeterNeedle = React.createClass({
+  render: function render () {
+    return d('div', {
+      className: style.meterNeedle,
+      style: {
+        backgroundColor: this.props.color,
+        left: this.props.position + '%'
+      }
+    })
   }
 })
 
